0b24856e9c8d5690e95d8f9edf6540885aa37dda
Peter Gombos <pepegombos@gmail.com>
2016-10-28 22:35:52 +0200

    Using react-bootstrap for HeaderNavbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap'
 import './Header.scss'
 
 export const Header = ({ user, handleLogOut }) => (
-  <div>
-    <nav className='navbar navbar-full navbar-fixed-top'>
-      <IndexLink to='/' className='navbar-brand'>BarFlow Manager</IndexLink>
-      {user &&
-        <div>
-          <Link to='/counter'>Counter</Link>
-          {user.name}
-          <a onClick={handleLogOut}> Logout</a>
-        </div>
-      }
-    </nav>
-  </div>
+  <Navbar collapseOnSelect fixedTop>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <IndexLinkContainer to='/'><a href='#'>BarFlow Manager</a></IndexLinkContainer>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    {user &&
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <NavDropdown eventKey={1} title={user.name} id='basic-nav-dropdown'>
+          <LinkContainer to='/profile'><MenuItem eventKey={1.1}>Profile</MenuItem></LinkContainer>
+          <MenuItem divider />
+          <MenuItem eventKey={1.2} onSelect={handleLogOut}>Log out</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+    }
+  </Navbar>
 )
 
 Header.propTypes = {
