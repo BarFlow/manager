04a9b40044c757633d5c6851c31a45ed63abd7a6
Peter Gombos <pepegombos@gmail.com>
2016-10-29 21:46:41 +0200

    Sub headbar for CoreLayout

--- src/routes/Inventory/components/InventoryView.js
+++ src/routes/Inventory/components/InventoryView.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './InventoryView.scss'
+import SubHeader from '../../../components/SubHeader'
 
 export const InventoryView = () => (
   <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
+    <SubHeader className='col-sm-offset-3 col-md-offset-2'>
+      <h3>Inventory</h3>
+    </SubHeader>
+    <div className='main'>
+      <h4>Welcome!</h4>
+      <img
+        alt='This is a duck, because Redux!'
+        className='duck'
+        src={DuckImage} />
+    </div>
   </div>
 )
 
