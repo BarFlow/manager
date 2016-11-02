151aba8d509e135d6f5615b01596d32f35e95e5f
Peter Gombos <pepegombos@gmail.com>
2016-11-02 19:41:10 +0100

    Login form using FormField component

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { FormGroup, ControlLabel, HelpBlock, FormControl } from 'react-bootstrap'
 
-const Field = ({ field, label, description, formControl, type }) => (
-  <FormGroup className={'col-xs-12 col-md-6'}
+const Field = ({ field, label, description, formControl, type, className }) => (
+  <FormGroup className={className}
     validationState={field.touched && field.error && 'error'}>
     <ControlLabel>{label}</ControlLabel>
     {formControl ||
@@ -24,6 +24,7 @@ Field.propTypes = {
   label: React.PropTypes.string.isRequired,
   description: React.PropTypes.string,
   type: React.PropTypes.string,
+  className: React.PropTypes.string,
   formControl: React.PropTypes.element
 }
 
